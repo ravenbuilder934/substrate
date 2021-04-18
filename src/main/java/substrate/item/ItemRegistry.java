1277a60377516e77d638459f166dd874d37a16b5
Raven <sabrinajamie@outlook.com>
2021-04-17 22:05:00 -0500

    Add trapdoors

--- src/main/java/substrate/item/ItemRegistry.java
+++ src/main/java/substrate/item/ItemRegistry.java
@@ -13,12 +13,14 @@ public class ItemRegistry
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Substrate.MOD_ID);
 
     public static final RegistryObject<TallBlockItem> copperdoor_item = ITEMS.register("copperdoor", () -> new TallBlockItem(copperdoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
-
     public static final RegistryObject<TallBlockItem> aluminiumdoor_item = ITEMS.register("aluminiumdoor", () -> new TallBlockItem(aluminiumdoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
-
     public static final RegistryObject<TallBlockItem> steeldoor_item = ITEMS.register("steeldoor", () -> new TallBlockItem(steeldoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
-
     public static final RegistryObject<TallBlockItem> castirondoor_item = ITEMS.register("castirondoor", () -> new TallBlockItem(castirondoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
-
     public static final RegistryObject<TallBlockItem> rustydoor_item = ITEMS.register("rustydoor", () -> new TallBlockItem(rustydoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
+
+    public static final RegistryObject<BlockItem> coppertrapdoor_item = ITEMS.register("coppertrapdoor", () -> new BlockItem(coppertrapdoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
+    public static final RegistryObject<BlockItem> aluminiumtrapdoor_item = ITEMS.register("aluminiumtrapdoor", () -> new BlockItem(aluminiumtrapdoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
+    public static final RegistryObject<BlockItem> steeltrapdoor_item = ITEMS.register("steeltrapdoor", () -> new BlockItem(steeltrapdoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
+    public static final RegistryObject<BlockItem> castirontrapdoor_item = ITEMS.register("castirontrapdoor", () -> new BlockItem(castirontrapdoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
+    public static final RegistryObject<BlockItem> rustytrapdoor_item = ITEMS.register("rustytrapdoor", () -> new BlockItem(rustytrapdoor.get(), new Item.Properties().tab(ItemGroup.TAB_DECORATIONS)));
 }

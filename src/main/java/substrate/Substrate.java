7fdd8ff8c3325f362fec760ef6d7e1a8e0282f13
Raven <sabrinajamie@outlook.com>
2021-05-08 15:23:56 -0500

    Fix trapdoor holes

--- src/main/java/substrate/Substrate.java
+++ src/main/java/substrate/Substrate.java
@@ -1,15 +1,20 @@
 package substrate;
 
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("substrate")
 public class Substrate
 {
-
     public Substrate()
     {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -21,6 +26,18 @@ public class Substrate
     private void setup(final FMLCommonSetupEvent event)
     {
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void doClientStuff(final FMLClientSetupEvent event)
+    {
+        RenderTypeLookup.setRenderLayer(BlockRegistry.aluminiumtrapdoor.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.castirontrapdoor.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.coppertrapdoor.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.irontrapdoor.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.rustytrapdoor.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.steeltrapdoor.get(), RenderType.translucent());
     }
 
 }
